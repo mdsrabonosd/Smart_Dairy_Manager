7b27b07df3292de0b9141788f2ba32740be56b12
MdSrabon <mdsrabon.cse@gmail.com>
2025-12-03 01:21:59 +0600

    vaccine apply list

--- Smart Dairy Manager/Controllers/VaccineApplyController.cs
+++ Smart Dairy Manager/Controllers/VaccineApplyController.cs
@@ -1,20 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Smart_Dairy_Manager.Data;
+using Smart_Dairy_Manager.Data_model;
 
 namespace Smart_Dairy_Manager.Controllers
 {
     public class VaccineApplyController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
+        public VaccineApplyController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+        [HttpGet]
         public IActionResult ApplyCreate()
         {
             return View();
         }
+        [HttpPost]
+
+        public IActionResult ApplyCreate(VaccineApply obj)
+        {
+            var data = _dbContext.VaccineApplies.Add(obj);
+            _dbContext.SaveChanges();
+            return RedirectToAction("ApplyCreate");
+        }
         public IActionResult ApplyList()
         {
-            return View();
+            var data = _dbContext.VaccineApplies.ToList();
+
+            return View(data);
         }
 
     }
